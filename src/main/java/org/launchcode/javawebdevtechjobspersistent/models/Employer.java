930072c48cf2ea06aa6c030f10c9a1d6cf5cd182
abroksa <slipstitched@gmail.com>
2021-03-25 15:42:37 -0500

    added a jobs field to employer class

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +14,10 @@ public class Employer extends AbstractEntity {
     @NotNull
     @NotBlank
     private String location;
+    //part 3 step 1.1 and 1.2 do i need "mapped by"?
+    @OneToMany
+    @JoinColumn
+    private final List<Job> events = new ArrayList<>();
 
     public Employer() {}
 
